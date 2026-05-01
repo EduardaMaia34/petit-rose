89ebb44e8e8b87ec76a010bcc1c75ffad2e92b03
EduardaMaia34 <eduardamfmcavalcanti@gmail.com>
2026-05-01 11:15:29 -0300

    feat: add SecurityConfiguration e SecurityFilter

--- petitrose/src/main/java/com/projeto/petitrose/models/Usuario.java
+++ petitrose/src/main/java/com/projeto/petitrose/models/Usuario.java
@@ -1,7 +1,13 @@
 package com.projeto.petitrose.models;
 
 import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -11,10 +17,11 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
 
+
 @Entity
 @Data //getters e setters
 @Table(name="Usuario")
-public class Usuario implements Serializable{
+public class Usuario implements Serializable, UserDetails{
     private static final long serialVersionUID = 1L; //camada extra de seguranca
     
     @Id
@@ -31,4 +38,46 @@ public class Usuario implements Serializable{
     private Boolean gerente = false; //valor padrao
 
 
+    //user detail - padrao
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        // Se o usuário for gerente, ele terá permissões de ADMIN e USER.
+        // Se não for, terá apenas permissão de USER.
+        if (Boolean.TRUE.equals(this.gerente)) {
+            return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_USER"));
+        }
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        // O Spring Security chama getPassword(), mas retorna o seu atributo 'senha'
+        return this.senha;
+    }
+
+    @Override
+    public String getUsername() {
+        // O Spring Security chama getUsername(), mas retorna o seu atributo 'email'
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true; // Conta ativa e não expirada
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true; // Conta não bloqueada
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true; // Credenciais válidas
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true; // Usuário ativo
+    }
 }
---
+++ petitrose/src/main/java/com/projeto/petitrose/repositories/UsuarioRepository.java
@@ -0,0 +1,13 @@
+package com.projeto.petitrose.repositories;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.projeto.petitrose.models.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, UUID>{
+    
+    UserDetails findByEmail(String email);
+}
