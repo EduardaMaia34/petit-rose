1c9106fec5632d1653c6e9b40053fd3c31247181
Mariana <marigsmnds@gmail.com>
2026-05-13 21:45:47 -0300

    Cadastrar usuario e login front e back conectados

--- frontend/petitrose-frontend/src/api/auth.ts
+++ frontend/petitrose-frontend/src/api/auth.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080/usuarios' // URL do seu UsuarioController
+    baseURL: 'http://localhost:8081/usuarios' // URL do seu UsuarioController
 });
 
 export const loginRequest = async (email: string, senha: string) => {
