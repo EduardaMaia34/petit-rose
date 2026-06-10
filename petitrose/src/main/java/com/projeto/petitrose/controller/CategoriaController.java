58216fba4ee90bfbed4e2ee84d31c263255ba2c3
EduardaMaia34 <eduardamfmcavalcanti@gmail.com>
2026-06-10 17:46:05 -0300

    produto e categoria funcionando

--- petitrose/src/main/java/com/projeto/petitrose/controller/CategoriaController.java
+++ petitrose/src/main/java/com/projeto/petitrose/controller/CategoriaController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/categorias")
+@RequestMapping("/categorias")
 public class CategoriaController {
 
     @Autowired
